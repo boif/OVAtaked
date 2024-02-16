4775c594c5b99cbb9300473a380f5e986e3934c7
boif3x <boif3x@gmail.com>
2024-02-16 14:47:25 +0300

    1step celery

--- OVA/views.py
+++ OVA/views.py
@@ -5,7 +5,7 @@ from OVA.forms import RegisterForm, AddImage, ProfileForm, ContactForm
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 from django.views.generic import CreateView
-from OVA.models import send
+from OVA.service import send
 
 
 
